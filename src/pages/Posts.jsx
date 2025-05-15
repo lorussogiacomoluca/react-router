b87a799529271de435f3036acac65ef79ebd4b49
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-15 15:48:12 +0200

    [UPD] Implemented `getData` function to retrieve post data from the API

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,6 +1,14 @@
+import axios from "axios";
 import React from "react";
 
 const Posts = () => {
+  const getData = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => console.log(response.data))
+      .catch((error) => console.error(error));
+  };
+
   return (
     <div>
       <h1>Posts</h1>
