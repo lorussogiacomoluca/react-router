d812caf8afd1e608dbf67c5d7a4cd5600cabba83
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-15 16:02:46 +0200

    [UPD] Utilized `useState` hook to store and manage all posts and tests in the application

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,11 +1,15 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Posts = () => {
+  const [posts, setPosts] = useState([]);
   const getData = () => {
     axios
       .get("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => console.log(response.data))
+      .then((response) => {
+        console.log(response.data);
+        setPosts(response.data);
+      })
       .catch((error) => console.error(error));
   };
 
@@ -16,6 +20,9 @@ const Posts = () => {
   return (
     <div>
       <h1>Posts</h1>
+      {posts.map((post, index) => (
+        <h5 key={post.id}>{post.title}</h5>
+      ))}
     </div>
   );
 };
