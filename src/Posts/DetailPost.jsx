e6459826648516a1923ef788d1e72578d540145f
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-16 15:12:38 +0200

    [UPD] Save id params in id variable

--- src/Posts/DetailPost.jsx
+++ src/Posts/DetailPost.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 const DetailPost = () => {
+  //Save id params in id variable
+  const { id } = useParams();
   return (
     <div>
       <h1>Post Detail</h1>
