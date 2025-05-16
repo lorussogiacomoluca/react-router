4d44d1603a6f07a95246fcc7662731dad204054b
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-16 15:16:21 +0200

    [UPD] Created useState variable to get Post Object

--- src/Posts/DetailPost.jsx
+++ src/Posts/DetailPost.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
 const DetailPost = () => {
   //Save id params in id variable
   const { id } = useParams();
+  // Creazione variaible di stato
+  const [post, setPost] = useState({});
+
   return (
     <div>
       <h1>Post Detail</h1>
